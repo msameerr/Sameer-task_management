137427fb96a04716e0dfe708384f144130807043
sameer arshad <arshadsameer820@gmail.com>
2025-03-24 12:22:14 +0500

    Initial Commit

---
+++ task_management.client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontSize: '24px',
+            fontWeight: 'bold'
+        }}>
+            Task Management System
+        </div>
+    );
+};
+
+export default App;
